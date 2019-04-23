8747fbcc7b30857205ae067aa71e569294c7e74f
dingkun <1126572821@qq.com>
2019-04-23 16:09:59 +0800

    zdk

--- src/menu/aside.js
+++ src/menu/aside.js
@@ -1,10 +1,10 @@
 // 菜单 侧边栏
 export default [
-  // {
-  //   path: '/index',
-  //   title: '首页',
-  //   icon: 'home'
-  // },
+  {
+    path: '/index',
+    title: '首页',
+    icon: 'home'
+  },
   {
     title: '渠道报表系统',
     icon: 'file-text-o ',
